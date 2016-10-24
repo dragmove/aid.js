9e92306d0726a9d1040381b7b8ff76e0158bbd9a
김현석 <vuild@ncsoft.com>
2016-10-24 15:09:23 +0900

    edit string.getUriCombinedParams function.

--- js/src/aid.js
+++ js/src/aid.js
@@ -712,7 +712,7 @@
     }
     if (str === '') return uri;
 
-    uri = uri + '?' + str.substr(1);
+    uri = (uri.indexOf('?') >= 0) ? (uri + str) : (uri + '?' + str.substr(1));
     return uri;
   };
 
