b00292ebc4be5040cfa42351217a9fc82336a1e0
dragmove <dragmove@naver.com>
2017-06-04 01:24:13 +0900

    add math.degreeToRadian, math.radianToDegree functions

--- js/src/aid.js
+++ js/src/aid.js
@@ -1890,6 +1890,42 @@
   };
 
   /**
+   * convert degree to radian.
+   *
+   * @static
+   * @method degreeToRadian
+   * @param {Number} number
+   * @returns {Number} return number
+   * @example
+   * console.log( aid.math.degreeToRadian(90) ); // 1.5707963267948966
+   */
+  math.degreeToRadian = function (degree) {
+    if (!aid.isNumber(degree)) {
+      throw new TypeError('math.degreeToRadian() requires Number parameter.');
+    }
+
+    return degree * Math.PI / 180;
+  };
+
+  /**
+   * convert radian to radian.
+   *
+   * @static
+   * @method radianToDegree
+   * @param {Number} number
+   * @returns {Number} return number
+   * @example
+   * console.log( aid.math.radianToDegree(1.5707963267948966) ); // 90
+   */
+  math.radianToDegree = function (radian) {
+    if (!aid.isNumber(radian)) {
+      throw new TypeError('math.radianToDegree() requires Number parameter.');
+    }
+
+    return radian * 180 / Math.PI;
+  };
+
+  /**
    * days name
    *
    * @static
